92b589cad741fe021f91b4578923dd5281f18d93
Jayesh Lata <jayesh@playsimple.com>
2015-09-10 21:00:00 +0530

    initial commit

---
+++ FTW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FTW
+//
+//  Created by Jayesh Lata on 9/10/15.
+//  Copyright (c) 2015 Jayesh Lata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
